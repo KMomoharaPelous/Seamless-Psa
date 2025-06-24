ff42eb2d336efe0c0322b8f72a4e75d2ddb6dc60
Kichelle Momohara-Pelous <87834614+KMomoharaPelous@users.noreply.github.com>
2025-06-24 07:36:26 -1000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config();
